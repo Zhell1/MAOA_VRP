c8e40ee02536fc3f4c6c52fb44b401038c9341f3
thomasz <thomas.zanivan@gmail.com>
2019-01-04 16:25:06 +0100

    second refactoring

--- Tutorial_CPLEX-SCIP_enonce/CompactMIP_CPLEX/CompactMIP_BinPacking_Projet/CompactMIP_BinPacking_Projet.cpp
+++ Tutorial_CPLEX-SCIP_enonce/CompactMIP_CPLEX/CompactMIP_BinPacking_Projet/CompactMIP_BinPacking_Projet.cpp
@@ -35,87 +35,15 @@ void print_all_tournees(vector<vector<int>> tournees, C_Graph* G) {
   cout << "\t total_cost = " << G->get_VRP_cost(tournees) << endl;
 }
 
-int main (int argc, char**argv){
-  //////////////////////////////
-  ///////// PARAMETERS /////////
-  //////////////////////////////
-  bool relaxedPLNE_activateprint  = false;
-  bool relaxedPLNE_activateoutput = false;
-  ////////////////////////////// end of parameters
-
-  vector<int> solution_vec;
-
-  if(argc!=2){
-    cerr<<"usage: "<<argv[0]<<" filename.vrp"<<endl; 
-    return 1;
-  }
-  string filename = argv[1];
-
-  C_Graph* G = parseVRPfile(filename, relaxedPLNE_activateprint);
-  //C_node* cnode = graph_ptr->get_node_by_id(1);    //exemple d'utilisation
-  //(*G).nb_nodes;    // G->nb_nodes;                //both mean the same here
-  //C_node* cnode  = (*G).get_node_by_id_startat1(1);   //example
-
-  //on solve par PLNE après avoir relaxé la contrainte de m tournées
-  int nb_box_used = solve_relaxedPLNE(G, filename, &solution_vec, relaxedPLNE_activateprint, relaxedPLNE_activateoutput); //last paramter is write_outputs?, befre-last is print?
-
-  cout << endl;
-  //print solution found by relaxed PLNE
-  cout << "NB BOXES USED : " << nb_box_used << endl;
-  cout << "SOLUTION : ";
-  for(int i = 0; i < solution_vec.size(); i++)
-        cout << solution_vec.at(i) << " " ;
-  cout << endl;
-
-  // la solution trouvée ne contient que des valeurs qui se suivent à partir de 1 : 1,2,3,4,5...
-
-  /////////////////////////////////////////////////////////////////////////////////////
-  //////////////// métaheuristique itérative par voisinage ////////////////////////////
-  /////////////////////////////////////////////////////////////////////////////////////
-
-  /*
-    A l’issue d’une étape initiale, on obtient une solution réalisable.
-    Une métaheuristique itérative peut alors être utilisée sur la base de plusieurs voisinages:
-    - les voisinages classiques du TSP (2-opt) pour améliorer chaque tournée indépendemment.
-    - la possiblité pour un client de changer de tournées
-    - supprimer une tournée vide
-    - éventuellement ajouter une tournée vide 
-
-    OBJ: minimiser la somme toale des couts des arcs utilisés (sous contrainte de capacité des véhicules)
-
-    TODO:
-        on peut décrire une tournée par une liste de sommets (i0, i1, ...ip) comme le graphe est complet.
-        tous les circuits commencent par le sommet 0 et finissent au sommet 0
-          
-        -> fonction : voisinage "possibilité pour un client de changer de tournées"
-        -> alterner entre les 2 heuristiques précédentes jusqu'à ne plus avoir d'amélioration (mais commencer par le client qui change de tournée)
-        -> peut-être ajouter métaheuristique en ajoutant une tournée vide au début qu'on supprime ensuite ?
-  */
-  //création du vecteur de vecteurs contenant toutes les tournées (sans le sommet 0):
-  vector<vector<int>> tournees;
-  //pour chacune des tournées
-  for(int i = 0; i < nb_box_used; i++) {
-      vector<int> curr_tournee;
-      //on lit tout le vecteur solution
-      for(int j = 0; j < solution_vec.size(); j++){
-          //on extrait les sommets de la tournée à enregistrer
-          if(solution_vec.at(j) == i+1) {
-              curr_tournee.push_back(j+1); //+1 pour commencer à 1 au lieu de 0
-          }
-      }
-      tournees.push_back(curr_tournee);
-  }
-  cout << endl;
-  //on affiche toutes les tournées et leurs couts:
-  print_all_tournees(tournees, G);
-
-  //voisinage 2opt pour optimiser chaque tournée indépendament
+void optimize_2opt_internalRoutes(vector<vector<int>> *tournees, C_Graph* G) {
+  int nb_box_used = tournees->size();
+   //voisinage 2opt pour optimiser chaque tournée indépendament
   cout << endl <<  "*** starting 2opt optimization of internal routes ***"<< endl;
   bool print_alldebug_2opt = false;
 
   //on parcours toutes les tournées une par une : 
   for(int ibox = 0; ibox < nb_box_used; ibox++) {
-    vector<int> *tournee = &(tournees.at(ibox)); // passe par un pointeur pour pouvoir modifier direct les valeurs
+    vector<int> *tournee = &(tournees->at(ibox)); // passe par un pointeur pour pouvoir modifier direct les valeurs
     //tournee->at(0) = 1000; cout << tournees.at(i).at(0) << " ???"<< endl; // test de modification OK
     float initial_cost = G->get_route_cost(*tournee);
     if(print_alldebug_2opt)cout << "tournee #"<<ibox<<" initial cost = " << initial_cost << endl; 
@@ -171,9 +99,86 @@ int main (int argc, char**argv){
     }
     cout << "\ttournee #"<<ibox<<"  cost : "<< initial_cost <<" -> " << G->get_route_cost(*tournee) << endl; 
   }
-  cout<<endl;
+}
+
+int main (int argc, char**argv){
+  //////////////////////////////
+  ///////// PARAMETERS /////////
+  //////////////////////////////
+  bool relaxedPLNE_activateprint  = false;
+  bool relaxedPLNE_activateoutput = false;
+  ////////////////////////////// end of parameters
+
+  vector<int> solution_vec;
+
+  if(argc!=2){
+    cerr<<"usage: "<<argv[0]<<" filename.vrp"<<endl; 
+    return 1;
+  }
+  string filename = argv[1];
+
+  C_Graph* G = parseVRPfile(filename, relaxedPLNE_activateprint);
+  //C_node* cnode = graph_ptr->get_node_by_id(1);    //exemple d'utilisation
+  //(*G).nb_nodes;    // G->nb_nodes;                //both mean the same here
+  //C_node* cnode  = (*G).get_node_by_id_startat1(1);   //example
+
+  //on solve par PLNE après avoir relaxé la contrainte de m tournées
+  int nb_box_used = solve_relaxedPLNE(G, filename, &solution_vec, relaxedPLNE_activateprint, relaxedPLNE_activateoutput); //last paramter is write_outputs?, befre-last is print?
+
+  cout << endl;
+  //print solution found by relaxed PLNE
+  cout << "NB BOXES USED : " << nb_box_used << endl;
+  cout << "SOLUTION : ";
+  for(int i = 0; i < solution_vec.size(); i++)
+        cout << solution_vec.at(i) << " " ;
+  cout << endl;
+  // remarque : la solution trouvée ne contient que des valeurs qui se suivent à partir de 1 : 1,2,3,4,5...
+
+  /////////////////////////////////////////////////////////////////////////////////////
+  //////////////// métaheuristique itérative par voisinage ////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////
+
+  /*
+    A l’issue d’une étape initiale, on obtient une solution réalisable.
+    Une métaheuristique itérative peut alors être utilisée sur la base de plusieurs voisinages:
+    - les voisinages classiques du TSP (2-opt) pour améliorer chaque tournée indépendemment.
+    - la possiblité pour un client de changer de tournées
+    - supprimer une tournée vide
+    - éventuellement ajouter une tournée vide 
+
+    OBJ: minimiser la somme toale des couts des arcs utilisés (sous contrainte de capacité des véhicules)
+
+    TODO:
+        on peut décrire une tournée par une liste de sommets (i0, i1, ...ip) comme le graphe est complet.
+        tous les circuits commencent par le sommet 0 et finissent au sommet 0
+          
+        -> fonction : voisinage "possibilité pour un client de changer de tournées"
+        -> alterner entre les 2 heuristiques précédentes jusqu'à ne plus avoir d'amélioration (mais commencer par le client qui change de tournée)
+        -> peut-être ajouter métaheuristique en ajoutant une tournée vide au début qu'on supprime ensuite ?
+  */
+  //création du vecteur de vecteurs contenant toutes les tournées (sans le sommet 0):
+  vector<vector<int>> tournees;
+  //pour chacune des tournées
+  for(int i = 0; i < nb_box_used; i++) {
+      vector<int> curr_tournee;
+      //on lit tout le vecteur solution
+      for(int j = 0; j < solution_vec.size(); j++){
+          //on extrait les sommets de la tournée à enregistrer
+          if(solution_vec.at(j) == i+1) {
+              curr_tournee.push_back(j+1); //+1 pour commencer à 1 au lieu de 0
+          }
+      }
+      tournees.push_back(curr_tournee);
+  }
+
   //on affiche toutes les tournées et leurs couts:
-  print_all_tournees(tournees, G);
+  cout<<endl; print_all_tournees(tournees, G);
+
+  //voisinage 2opt pour optimiser chaque tournée indépendament
+  optimize_2opt_internalRoutes(&tournees, G);
+  
+  //on affiche toutes les tournées et leurs couts:
+  cout<<endl; print_all_tournees(tournees, G);
 
   ///////////////////////////////////////////////////////////////////////////////////// fin métaheuristique itérative par voisinage
   
